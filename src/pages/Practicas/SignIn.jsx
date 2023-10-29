0c25db859ada6e71262154c9adda4667075698a8
Chii1914 <maximiliano.aguirre@alumnos.uv.cl>
2023-10-29 15:57:47 -0300

    Se inicia el Home

--- src/pages/Practicas/SignIn.jsx
+++ src/pages/Practicas/SignIn.jsx
@@ -1,25 +1,22 @@
 import React from "react";
 import PageContainer from "../../components/container/PageContainer";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Container, Paper } from "@mui/material";
+import ButtonsSign from "./Components/ButtonsLog";
 
-
-function Home() {
+function SignIn() {
   return (
-    <PageContainer title="Pagina inicio" description="aaaaaaaaaaaaaaaaa">
+    <PageContainer title="Prácticas - Postulación a practicas profesionales | Administración Pública" description="Sistema de prácticas profesionales">
       <Box
         sx={{
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-
           width: "100%",
           backgroundImage: 'url(https://cdn.discordapp.com/attachments/1145537724644864134/1166227278720671775/image.png?ex=6549b8ca&is=653743ca&hm=fe863285f7e6c37ea2b2c8d499f3f9b9d7b92f615ef8f34692dd7c81586a0dec&)',
-
           backgroundSize: "cover",
           backgroundRepeat: "no-repeat",
           backgroundPosition: "center center",
           minHeight: "983px",
-          width: "654",
           padding: "100px",
         }}
         minHeight={700}
@@ -31,32 +28,31 @@ function Home() {
           justifyContent={"center"}
           alignItems="center"
         >
+         <Box sx ={{
+          bgcolor: 'white',
+          color: 'black',
+          boxShadow: 4,
+          justifyContent: 'center',
+          width: '100%',
+          alignItems: 'center',
+         }}>
           <Typography
-            variant="h1"
+            variant="h3"
             component="h1"
             sx={{
               color: "black",
-              textShadow: "4px 4px 8px #FFFFFF",
               fontFamily: "monospace",
               fontWeight: 700,
               letterSpacing: ".3rem",
             }}
           >
+            Postulación a Prácticas Profesionaless
+            </Typography>
+            
+            <ButtonsSign/>
+            
           
-          </Typography>
-          <Typography
-            variant="h4"
-            component="h2"
-            sx={{
-              color: "black",
-              textShadow: "2px 2px 4px #FFFFFF",
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-            }}
-          >
-          </Typography>
-          
+          </Box> 
         </Box>
       </Box>
       <Box
@@ -66,23 +62,11 @@ function Home() {
         gap={5}
         alignItems="center"
       >
-        <Typography
-          variant="h4"
-          component="h2"
-          sx={{
-            fontFamily: "monospace",
-            fontWeight: 700,
-            letterSpacing: ".3rem",
-            color: "white",
-            textShadow: "2px 2px 4px #000000",
-          }}
-        >
-          
-        </Typography>
+        
 
       </Box>
     </PageContainer>
   );
 }
 
-export default Home;
+export default SignIn;
