505489676fe7c85a8566416fa47ac864d242f05b
Chii1914 <maximiliano.aguirre@alumnos.uv.cl>
2024-03-14 18:25:11 -0300

    Refactor login component and handle login functionality

--- src/pages/Practicas/Login/Components/Login_component.jsx
+++ src/pages/Practicas/Login/Components/Login_component.jsx
@@ -9,30 +9,50 @@ import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import axios from "axios";
 import ReplyIcon from "@mui/icons-material/Reply";
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
 
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
-
 export default function SignIn() {
   const navigate = useNavigate();
 
-  
+  const handleLogin = async () => {
+    if (Cookies.get("token")) {
+      try {
+        const res = await axios.get("http://localhost:3000/api/auth/verify", {
+          headers: {
+            Authorization: `Bearer ${Cookies.get("token")}`,
+          },
+        });
+        navigate("/practicas/success");
+      } catch (error) {
+        Cookies.remove("token");
+        window.location.href =
+          "http://localhost:3000/api/auth/google?prompt=select_account";
+      }
+    } else {
+      Cookies.remove("token");
+      window.location.href =
+        "http://localhost:3000/api/auth/google?prompt=select_account";
+    }
+  };
+  /*
   const handleLogin = () => {
     if (Cookies.get('token')) {
+      verifyUser();
       navigate("/practicas/success");
     }else{
       window.location.href = "http://localhost:3000/api/auth/google?prompt=select_account";    }
   }
-  
+  */
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
       <Box
         sx={{
-          
           backgroundSize: "cover",
           backgroundPosition: "center",
           display: "flex",
@@ -40,8 +60,7 @@ export default function SignIn() {
           alignItems: "center",
         }}
       >
-       <Button onClick={handleLogin}> Iniciar Sesión </Button>
-
+        <Button onClick={handleLogin}> Iniciar Sesión </Button>
       </Box>
     </ThemeProvider>
   );
