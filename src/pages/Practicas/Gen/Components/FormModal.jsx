4395c38666f9058f816dec76b6b72c809d4d0c97
Step <stephano.almendra@gmail.com>
2024-07-04 21:01:47 -0400

    -correo institucional no sea modificable - ningún campo requerido quede vacío -teléfono tenga exactamente 9 dígitos -errores de validación y mostrar mensajes adecuados

--- src/pages/Practicas/Gen/Components/FormModal.jsx
+++ src/pages/Practicas/Gen/Components/FormModal.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
@@ -7,224 +7,265 @@ import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import { Select, MenuItem, InputLabel, RadioGroup, FormControlLabel, Radio, FormLabel } from "@mui/material";
 
-const FormModal = ({ open, handleClose, inputs, handleChange, handleSubmit, style }) => (
-  <Modal open={open} onClose={handleClose}>
-    <Box sx={{ ...style, overflow: "auto", maxHeight: "90vh" }}>
-      <Typography id="modal-modal-title" variant="h6" component="h2">
-        Modificación data personal
-      </Typography>
-      <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="primerNombre"
-          label="Ingrese su primer nombre"
-          name="primerNombre"
-          autoComplete="primer_nombre"
-          autoFocus
-          value={inputs.primerNombre}
-          onChange={handleChange}
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          label="Ingrese su segundo nombre"
-          type="text"
-          name="segundoNombre"
-          id="segundoNombre"
-          value={inputs.segundoNombre}
-          onChange={handleChange}
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          name="apellidoPaterno"
-          label="Ingrese su primer apellido"
-          type="apellidoPaterno"
-          id="apellidoPaterno"
-          value={inputs.apellidoPaterno}
-          onChange={handleChange}
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          name="apellidoMaterno"
-          label="Ingrese su segundo apellido"
-          type="text"
-          id="apellidoMaterno"
-          value={inputs.apellidoMaterno}
-          onChange={handleChange}
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          name="run"
-          label="Ingrese su rut sin puntos ni guión"
-          type="number"
-          id="run"
-          value={inputs.run}
-          onChange={handleChange}
-          inputProps={{ pattern: "[0-9]*" }}
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          name="correoInstitucional"
-          label="Ingrese su correo institucional"
-          type="text"
-          id="correoInstitucional"
-          value={inputs.correoInstitucional}
-          onChange={handleChange}
-        />
-        <Box>
-          <Grid container spacing={3}>
-            <Grid item xs={6}>
-              <FormLabel id="sexo">Género</FormLabel>
-              <RadioGroup
-                defaultValue="femenino"
-                name="sexo"
-                onChange={handleChange}
-                value={inputs.sexo}
-              >
-                <FormControlLabel
-                  value="femenino"
-                  control={<Radio />}
-                  label="Mujer"
-                />
-                <FormControlLabel
-                  value="masculino"
-                  control={<Radio />}
-                  label="Hombre"
-                />
-                <FormControlLabel
-                  value="Otro"
-                  control={<Radio />}
-                  label="No Especifica"
-                />
-              </RadioGroup>
-            </Grid>
-            <Grid item xs={6}>
-              <InputLabel id="df">Dígito Verificador</InputLabel>
-              <Select
-                labelId="df"
-                id="df"
-                label="df"
-                name="df"
-                value={inputs.df}
-                onChange={handleChange}
-              >
-                <MenuItem value={"0"}>0</MenuItem>
-                <MenuItem value={"1"}>1</MenuItem>
-                <MenuItem value={"2"}>2</MenuItem>
-                <MenuItem value={"3"}>3</MenuItem>
-                <MenuItem value={"4"}>4</MenuItem>
-                <MenuItem value={"5"}>5</MenuItem>
-                <MenuItem value={"6"}>6</MenuItem>
-                <MenuItem value={"7"}>7</MenuItem>
-                <MenuItem value={"8"}>8</MenuItem>
-                <MenuItem value={"9"}>9</MenuItem>
-                <MenuItem value={"k"}>k</MenuItem>
-              </Select>
-            </Grid>
-            <Grid item xs={6}>
-              <InputLabel id="sede">Sede</InputLabel>
-              <Select
-                labelId="sede"
-                id="sede"
-                label="sede"
-                name="sede"
-                value={inputs.sede}
-                onChange={handleChange}
-              >
-                <MenuItem value={"Valparaíso"}>Valparaíso</MenuItem>
-                <MenuItem value={"Santiago"}>Santiago</MenuItem>
-              </Select>
-            </Grid>
-            <Grid item xs={6}>
-              <InputLabel id="anioIngreso">Año de Ingreso</InputLabel>
-              <Select
-                labelId="anioIngreso"
-                id="anioIngreso"
-                label="anioIngreso"
-                name="anioIngreso"
-                value={inputs.anioIngreso}
-                onChange={handleChange}
-              >
-                <MenuItem value="">
-                  <em>None</em>
-                </MenuItem>
-                {Array.from({ length: 17 }, (_, i) => (
-                  <MenuItem key={i} value={2008 + i}>
-                    {2008 + i}
+const FormModal = ({ open, handleClose, inputs, handleChange, handleSubmit, style }) => {
+  const [errors, setErrors] = useState({});
+
+  const validate = () => {
+    let tempErrors = {};
+    tempErrors.primerNombre = inputs.primerNombre ? "" : "Este campo es requerido.";
+    tempErrors.segundoNombre = inputs.segundoNombre ? "" : "Este campo es requerido.";
+    tempErrors.apellidoPaterno = inputs.apellidoPaterno ? "" : "Este campo es requerido.";
+    tempErrors.apellidoMaterno = inputs.apellidoMaterno ? "" : "Este campo es requerido.";
+    tempErrors.run = inputs.run ? "" : "Este campo es requerido.";
+    tempErrors.correoInstitucional = inputs.correoInstitucional ? "" : "Este campo es requerido.";
+    tempErrors.telefono = inputs.telefono ? "" : "Este campo es requerido.";
+    tempErrors.telefono = inputs.telefono && inputs.telefono.length === 9 ? "" : "El teléfono debe tener 9 dígitos.";
+
+    setErrors(tempErrors);
+    return Object.values(tempErrors).every((x) => x === "");
+  };
+
+  const handleSubmitWithValidation = (event) => {
+    event.preventDefault();
+    if (validate()) {
+      handleSubmit(event);
+    }
+  };
+
+  return (
+    <Modal open={open} onClose={handleClose}>
+      <Box sx={{ ...style, overflow: "auto", maxHeight: "90vh" }}>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          Modificación data personal
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSubmitWithValidation} sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="primerNombre"
+            label="Ingrese su primer nombre"
+            name="primerNombre"
+            autoComplete="primer_nombre"
+            autoFocus
+            value={inputs.primerNombre}
+            onChange={handleChange}
+            error={!!errors.primerNombre}
+            helperText={errors.primerNombre}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Ingrese su segundo nombre"
+            type="text"
+            name="segundoNombre"
+            id="segundoNombre"
+            value={inputs.segundoNombre}
+            onChange={handleChange}
+            error={!!errors.segundoNombre}
+            helperText={errors.segundoNombre}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="apellidoPaterno"
+            label="Ingrese su primer apellido"
+            type="text"
+            id="apellidoPaterno"
+            value={inputs.apellidoPaterno}
+            onChange={handleChange}
+            error={!!errors.apellidoPaterno}
+            helperText={errors.apellidoPaterno}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="apellidoMaterno"
+            label="Ingrese su segundo apellido"
+            type="text"
+            id="apellidoMaterno"
+            value={inputs.apellidoMaterno}
+            onChange={handleChange}
+            error={!!errors.apellidoMaterno}
+            helperText={errors.apellidoMaterno}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="run"
+            label="Ingrese su rut sin puntos ni guión"
+            type="text"
+            id="run"
+            value={inputs.run}
+            onChange={handleChange}
+            error={!!errors.run}
+            helperText={errors.run}
+            inputProps={{ pattern: "[0-9]*" }}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="correoInstitucional"
+            label="Ingrese su correo institucional"
+            type="text"
+            id="correoInstitucional"
+            value={inputs.correoInstitucional}
+            onChange={handleChange}
+            disabled
+            error={!!errors.correoInstitucional}
+            helperText={errors.correoInstitucional}
+          />
+          <Box>
+            <Grid container spacing={3}>
+              <Grid item xs={6}>
+                <FormLabel id="sexo">Género</FormLabel>
+                <RadioGroup
+                  defaultValue="femenino"
+                  name="sexo"
+                  onChange={handleChange}
+                  value={inputs.sexo}
+                >
+                  <FormControlLabel
+                    value="femenino"
+                    control={<Radio />}
+                    label="Mujer"
+                  />
+                  <FormControlLabel
+                    value="masculino"
+                    control={<Radio />}
+                    label="Hombre"
+                  />
+                  <FormControlLabel
+                    value="Otro"
+                    control={<Radio />}
+                    label="No Especifica"
+                  />
+                </RadioGroup>
+              </Grid>
+              <Grid item xs={6}>
+                <InputLabel id="df">Dígito Verificador</InputLabel>
+                <Select
+                  labelId="df"
+                  id="df"
+                  label="df"
+                  name="df"
+                  value={inputs.df}
+                  onChange={handleChange}
+                >
+                  <MenuItem value={"0"}>0</MenuItem>
+                  <MenuItem value={"1"}>1</MenuItem>
+                  <MenuItem value={"2"}>2</MenuItem>
+                  <MenuItem value={"3"}>3</MenuItem>
+                  <MenuItem value={"4"}>4</MenuItem>
+                  <MenuItem value={"5"}>5</MenuItem>
+                  <MenuItem value={"6"}>6</MenuItem>
+                  <MenuItem value={"7"}>7</MenuItem>
+                  <MenuItem value={"8"}>8</MenuItem>
+                  <MenuItem value={"9"}>9</MenuItem>
+                  <MenuItem value={"k"}>k</MenuItem>
+                </Select>
+              </Grid>
+              <Grid item xs={6}>
+                <InputLabel id="sede">Sede</InputLabel>
+                <Select
+                  labelId="sede"
+                  id="sede"
+                  label="sede"
+                  name="sede"
+                  value={inputs.sede}
+                  onChange={handleChange}
+                >
+                  <MenuItem value={"Valparaíso"}>Valparaíso</MenuItem>
+                  <MenuItem value={"Santiago"}>Santiago</MenuItem>
+                </Select>
+              </Grid>
+              <Grid item xs={6}>
+                <InputLabel id="anioIngreso">Año de Ingreso</InputLabel>
+                <Select
+                  labelId="anioIngreso"
+                  id="anioIngreso"
+                  label="anioIngreso"
+                  name="anioIngreso"
+                  value={inputs.anioIngreso}
+                  onChange={handleChange}
+                >
+                  <MenuItem value="">
+                    <em>None</em>
                   </MenuItem>
-                ))}
-              </Select>
+                  {Array.from({ length: 17 }, (_, i) => (
+                    <MenuItem key={i} value={2008 + i}>
+                      {2008 + i}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </Grid>
             </Grid>
-          </Grid>
+          </Box>
+          <TextField
+            margin="normal"
+            fullWidth
+            name="correoPersonal"
+            label="Correo Personal (Opcional)"
+            type="text"
+            id="correoPersonal"
+            value={inputs.correoPersonal}
+            onChange={handleChange}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="telefono"
+            label="Teléfono"
+            type="tel"
+            id="telefono"
+            value={inputs.telefono}
+            onChange={(e) => {
+              if (e.target.value === "" || /^[0-9]*$/.test(e.target.value)) {
+                handleChange(e);
+              }
+            }}
+            error={!!errors.telefono}
+            helperText={errors.telefono}
+            inputProps={{ maxLength: 9 }}
+          />
+          <InputLabel id="ultimoSemAprobado">Último Semestre Aprobado</InputLabel>
+          <Select
+            labelId="ultimoSemAprobado"
+            id="ultimoSemAprobado"
+            value={inputs.ultimoSemAprobado}
+            label="Último Semestre Aprobado"
+            name="ultimoSemAprobado"
+            onChange={handleChange}
+          >
+            <MenuItem value="Primer Semestre">Primer Semestre</MenuItem>
+            <MenuItem value="Segundo Semestre">Segundo Semestre</MenuItem>
+            <MenuItem value="Tercer Semestre">Tercer Semestre</MenuItem>
+            <MenuItem value="Cuarto Semestre">Cuarto Semestre</MenuItem>
+            <MenuItem value="Quinto Semestre">Quinto Semestre</MenuItem>
+            <MenuItem value="Sexto Semestre">Sexto Semestre</MenuItem>
+            <MenuItem value="Séptimo Semestre">Séptimo Semestre</MenuItem>
+            <MenuItem value="Octavo Semestre">Octavo Semestre</MenuItem>
+            <MenuItem value="Noveno Semestre">Noveno Semestre</MenuItem>
+            <MenuItem value="Décimo Semestre">Décimo Semestre</MenuItem>
+          </Select>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Modificar Data Personal
+          </Button>
         </Box>
-        <TextField
-          margin="normal"
-          fullWidth
-          name="correoPersonal"
-          label="Correo Personal"
-          type="correoPersonal"
-          id="correoPersonal"
-          value={inputs.correoPersonal}
-          onChange={handleChange}
-          defaultValue={null}
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          name="telefono"
-          label="Teléfono"
-          type="tel"
-          id="telefono"
-          value={inputs.telefono}
-          onChange={(e) => {
-            if (e.target.value === "" || /^[0-9]*$/.test(e.target.value)) {
-              handleChange(e);
-            }
-          }}
-        />
-        <InputLabel id="ultimoSemAprobado">Último Semestre Aprobado</InputLabel>
-        <Select
-          labelId="ultimoSemAprobado"
-          id="ultimoSemAprobado"
-          value={inputs.ultimoSemAprobado}
-          label="Último Semestre Aprobado"
-          name="ultimoSemAprobado"
-          onChange={handleChange}
-        >
-          <MenuItem value="Primer Semestre">Primer Semestre</MenuItem>
-          <MenuItem value="Segundo Semestre">Segundo Semestre</MenuItem>
-          <MenuItem value="Tercer Semestre">Tercer Semestre</MenuItem>
-          <MenuItem value="Cuarto Semestre">Cuarto Semestre</MenuItem>
-          <MenuItem value="Quinto Semestre">Quinto Semestre</MenuItem>
-          <MenuItem value="Sexto Semestre">Sexto Semestre</MenuItem>
-          <MenuItem value="Séptimo Semestre">Séptimo Semestre</MenuItem>
-          <MenuItem value="Octavo Semestre">Octavo Semestre</MenuItem>
-          <MenuItem value="Noveno Semestre">Noveno Semestre</MenuItem>
-          <MenuItem value="Décimo Semestre">Décimo Semestre</MenuItem>
-        </Select>
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          sx={{ mt: 3, mb: 2 }}
-        >
-          Modificar Data Personal
-        </Button>
       </Box>
-    </Box>
-  </Modal>
-);
+    </Modal>
+  );
+};
 
 export default FormModal;
